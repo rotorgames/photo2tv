a668102244df45d8368940a9ce0499a34f73c013
rotorgames <rotorgames@bk.ru>
2014-05-22 00:39:00 +0400

    Add getPicture
    Берем фото с камеры в форате base64

--- js/app.js
+++ js/app.js
@@ -1,5 +1,27 @@
 App = angular.module('App', ['ionic'])
 
-.controller('SignInCtrl', function($scope, $state) {
-	
+.config(function($stateProvider, $urlRouterProvider, $locationProvider) {
+
+  $stateProvider
+    .state('index', {
+      url: "/index",
+      templateUrl: "views/index.html",
+      controller: 'IndexCtrl'
+    })
+
+	//$locationProvider.html5Mode(true).hashPrefix('!');
+	$urlRouterProvider.otherwise("/index");
+
+})
+
+.controller('IndexCtrl', function($scope, $state) {
+	$scope.picture = "http://angular.ru/img/AngularJS-small.png";
+	$scope.getPicture = function(){
+		navigator.camera.getPicture(success,function(e){console.log(e)},{quality: 50, destinationType: Camera.DestinationType.DATA_URL })
+		function success (uri){
+			console.log(uri);
+			$scope.picture = "data:image/jpeg;base64,"+uri;
+			$scope.$digest();
+		}
+	}
 })
